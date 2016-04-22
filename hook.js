5c7769388d7d63a5d73484b849bfad9e2480b74a
Ido Cohen <ido.c.mail@gmail.com>
2016-04-22 12:22:45 +0300

    Create hook.js

---
+++ hook.js
@@ -0,0 +1,55 @@
+module['exports'] = function echoHttp(hook) {
+  
+  var request = require('request');
+  var token = hook.env.pageToken;
+
+  messaging_events = hook.params.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = hook.params.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      console.log(JSON.stringify(sender));
+      sendTextMessage(sender, "" + text.substring(0, 200));
+    }
+  }
+
+
+  function sendTextMessage(sender, text) {   
+    var words = text.split(" "); 
+
+    var dogrUrl = "";
+    for(var i = 0; i < words.length; i++) {
+    	dogrUrl = dogrUrl + '/' + words[i].replace(/[^\w\s]/gi, '');      
+	}    
+    dogrUrl = "http://dogr.io/wow/amaze" +dogrUrl+".png?split=false"
+    console.log(dogrUrl);
+	    
+  messageData = {
+    "attachment":{
+      "type":"image",
+      "payload":{
+        "url": dogrUrl
+      }
+    }
+  };
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+      console.log("response");
+      console.log(JSON.stringify(response));
+      if (error) {
+        console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+      }
+      hook.res.end();
+    });
+  }
+};
